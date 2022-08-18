1b7a6be2727acf04596c9dcd2ef5d1eaaebd6cc1
lee <sipossa@naver.com>
2022-08-18 17:16:45 +0900

    20220818

--- trn05/sgc.js
+++ trn05/sgc.js
@@ -18,8 +18,13 @@ $(function () {
     //https://github.com/aminta/fullPage.js-2.9.6 2버전 설명...
     //https://desipossa.github.io/backup-01/%ED%92%80%ED%8E%98%EC%9D%B4%EC%A7%80%20%EC%8B%A4%EC%8A%B5%20f01/fullpage01.html
 
-    $('.page02').on('wheel', function (e) {
+    $('.slide').on('wheel', function (e) {
         console.log(e.originalEvent.wheelDelta);
-        e.originalEvent.wheelDelta > 0 ? $.fn.fullpage.moveSlideLeft() : $.fn.fullpage.moveSlideRight()
+        if (e.originalEvent.wheelDelta > 0 && $(this).index() > 0) {
+            $.fn.fullpage.moveSlideLeft();
+        }
+        if (e.originalEvent.wheelDelta < 0 && $(this).index() < 2) {
+            $.fn.fullpage.moveSlideRight()
+        }
     })
 })
