6a5e007a01a87ae950b60b99640f9d33598a22ba
lee <sipossa@naver.com>
2022-08-19 11:01:35 +0900

    20220819

--- trn05/sgc.js
+++ trn05/sgc.js
@@ -5,16 +5,24 @@ $(function () {
         //navigation: true,
         css3: false,
         afterLoad: function (page, num) {
+            //애니메이션을 큐스텍에 담아서 딜레이 시킴...
             setTimeout(function () {
                 $('.section').eq(num - 1).addClass('on').siblings().removeClass('on');
             });
             $('.navBar li').eq(num - 1).addClass('on').siblings().removeClass('on');
             $('.this_page').text(TXT[num - 1]);
 
+            //부정연산자 사용
             num !== 1
                 ? $('.Header').addClass('on')
                 : $('.Header').removeClass('on')
         },
+    });
+
+
+    $('.basicSlider').slick({
+        arrows: false,
+        asNavFor: '.basicSlider',
     });
 
 })
